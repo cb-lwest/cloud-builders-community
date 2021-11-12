4a02099e7780b50e5ef6eff4ea08357283a1315a
cb-lwest <lwest@cloudbakers.com>
2021-11-12 16:21:54 -0500

    commented out destroy

--- terraform/examples/infra_at_scale/main.tf
+++ terraform/examples/infra_at_scale/main.tf
@@ -22,8 +22,28 @@ module "gke" {
   region             = var.region
   zones              = [var.zone]
   initial_node_count = 4
-  network            = "default"
-  subnetwork         = "default"
-  ip_range_pods      = ""
-  ip_range_services  = ""
+  network            = "svpc01"
+  subnetwork         = "subnet3-lwest"
+  ip_range_pods      = "gke-pods-cidr"
+  ip_range_services  = "gke-services-cidr"
+  
+  node_pools = [
+    {
+      name                      = "default-node-pool"
+      machine_type              = "e2-micro"
+      node_locations            = "us-central1-b,us-central1-c"
+      min_count                 = 1
+      max_count                 = 100
+      local_ssd_count           = 0
+      disk_size_gb              = 100
+      disk_type                 = "pd-standard"
+      image_type                = "COS"
+      auto_repair               = true
+      auto_upgrade              = true
+      service_account           = "prod-lwest-workloads@c4o-prod-lwest.iam.gserviceaccount.com"
+      preemptible               = false
+      initial_node_count        = 10
+    },
+  ]
+
 }
